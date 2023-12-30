2de7029cc951b4c2fe846c42b303be20be8487a7
Clmt2707 <clement.deboeuf@gmail.com>
2023-12-30 11:19:05 +0100

    Ajout commentaires

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,19 +1,36 @@
+///////////////////////////////////// FETCH WORKS ////////////////////////////////////////
 
 //Récupération des travaux via l'api 
 //Fetch renvoie une promesse, lorsqu'on recoit la reponse du serveur
+//Gestion des erreurs avec catch
 
-//Puis création de la fonction qui génère la galerie en créant des elements dans le DOM
+const fetchWorks = async () => {
+    try {
+        const reponse = await fetch("http://localhost:5678/api/works");
+        //une fois la réponse recue et convertit en json,on la stocke dans works
+        //JSon: conversion en chaîne de caractère
+        let works = await reponse.json();
+        return works;
+    } catch (error) {
+        console.error("Erreur: ", error);
+        throw error;
+    }
+};
+/////////////////////////////////////  CRÉATION GALERIE ///////////////////////////////////////
+
+//Création de la fonction qui génère la galerie en créant des elements dans le DOM
 //et qui fait le lien éléments parents - éléments enfants 
+let works = await fetchWorks();
 
-const reponse = await fetch('http://localhost:5678/api/works');
-let works = await reponse.json();
-
-function genererGallery(works) {
+async  function genererGallery(works) {
+    //on récupère la division dans laquelle on veut introduire les projets
+    //document. : méthode qui permet de manipuler le DOM
     const sectionGallery = document.querySelector(".gallery");
 
     for (let i = 0; i < works.length; i++) {
         const project = works[i];
 
+        //création des éléments
         const newFigure = document.createElement("figure");
         newFigure.dataset.id = project.id;
 
@@ -22,24 +39,27 @@ function genererGallery(works) {
         const newFigcaption = document.createElement("figcaption");
         newFigcaption.innerText = project.title;
 
+        //lien parents/enfants
         newFigure.appendChild(newImage);
         newFigure.appendChild(newFigcaption);
         sectionGallery.appendChild(newFigure);
     }
 }
-
+//Appel de la fonction pour afficher les projets
 genererGallery(works);
 
-//Création dans le DOM des éléments filtres
 
-//création d'un tableau
+
+///////////////////////////////////Création dans le DOM des éléments filtres////////////////////////////////
+
+//création d'un tableau(possibilité d'ajouter un filtre ici)
 var buttonData = [
     {class: "active", name: "Tous"},
     {class: "btn-Objet", name: "Objets"},
     {class: "btn-App", name: "Appartements"},
     {class: "btn-Hotel", name: "Hôtels & restaurants"}
 ]
-//on boucle pour ajouter les classes,textes et autres aux boutons
+//on boucle pour ajouter les classes,textes... aux boutons
 for (let j = 0; j < buttonData.length; j++) {
     let button = document.createElement("button");
     button.classList.add(buttonData[j].class);
@@ -54,9 +74,10 @@ for (let j = 0; j < buttonData.length; j++) {
 
     //Gestion du background du bouton séléctionné
     button.addEventListener("click", function(event) {
+        //on stocke l'évenement dans clickedButton
         let clickedButton = event.target;
-       
-        //on vérifie sur le bouton cliqué conient la classe active
+
+        //on vérifie si le bouton cliqué contient la classe active
         for (let k = 0; k < buttonData.length; k++) {
             let button = document.querySelector(".active");
             if (button && button.classList.contains("active")) {
@@ -67,21 +88,26 @@ for (let j = 0; j < buttonData.length; j++) {
     });
 }
 
+///////////////////////////////////////// FONCTIONS FILTERS ///////////////////////////////////
+
 //fonctions pour filter les projets, utilisation de filter
 const filterAll = document.querySelector(".active");
 filterAll.addEventListener("click",function(){
     const allFiltres = works.filter(() => {
         return genererGallery;
     })
+    //remise à zéro de la galerie puis affichage
     document.querySelector(".gallery").innerHTML = "";
     genererGallery(allFiltres);
 })
 
 const filterButtonO = document.querySelector(".btn-Objet");
 filterButtonO.addEventListener("click", function() {
-    const objectFiltres = works.filter(function(project) {
+        //récupération des projets contenant le name Objets
+        const objectFiltres = works.filter(function(project) {
         return project.category.name === "Objets";
     });
+    //remise à zéro de la galerie puis affichage
     document.querySelector(".gallery").innerHTML = "";
     genererGallery(objectFiltres);
 });
@@ -90,8 +116,10 @@ filterButtonO.addEventListener("click", function() {
 const filterButtonApp = document.querySelector(".btn-App");
 filterButtonApp.addEventListener("click", function(){
     const appFiltres = works.filter(function(project) {
+        //récupération des projets contenant le name Appartements 
         return project.category.name === "Appartements";
     });
+    //remise à zéro de la galerie puis affichage
     document.querySelector(".gallery").innerHTML = "";
     genererGallery(appFiltres);
 });
@@ -100,14 +128,17 @@ filterButtonApp.addEventListener("click", function(){
 const filterButtonHotel = document.querySelector(".btn-Hotel");
 filterButtonHotel.addEventListener("click", function() {
     const hotelFiltres = works.filter(function(project) {
+        //récupération des projets contenant le name Hotels & restaurants
         return project.category.name === "Hotels & restaurants";
     });
+    //remise à zéro de la galerie puis affichage
     document.querySelector(".gallery").innerHTML = "";
     genererGallery(hotelFiltres);
 })
 
+////////////////////////////////////////////// GALERIE MODALE  //////////////////////////////////////////
 
-///////// Ajout de la galerie à la modale /////////
+//Même principe que la fonction genererGallery, ajout option suppression
 
 async function galerieModale(works) {
     const modalGallery = document.querySelector(".affiche-gallery");
@@ -136,6 +167,7 @@ async function galerieModale(works) {
         btntrash.addEventListener("click", (event) => {
             event.preventDefault();
             deleteProjet(work.id);
+            console.log("Work deleted");
         })
     }
 }
@@ -143,7 +175,7 @@ async function galerieModale(works) {
 galerieModale(works);
 
 
-/////// MODE ADMINISTRATEUR ////////
+////////////////////////////////////////// MODE ADMINISTRATEUR /////////////////////////////////////////
 
 //on récupère les éléments cachés et on leur attribue display:none
 const elementHidden = document.querySelectorAll(".hide");
@@ -153,9 +185,11 @@ elementHidden.forEach(element => {
 
 //création et affichage page admin
 function AdminPage() {
+    //on récupère id et token dans le localstorage
     const userId = localStorage.getItem("userId");
     const token = localStorage.getItem("token");
     
+    // vérification de leur présence, si oui on inverse l'affichage des éléments cachés/non cachés
     if (userId && token) {
         const hiddenElements = document.querySelectorAll(".hide");
         const ActiveElements = document.querySelectorAll(".not-hidden");
@@ -169,11 +203,9 @@ function AdminPage() {
     }
 }
 
-
 AdminPage();
 
-
-//Passage d'une modale à l'autre 
+//////////////////////////////////////////PASSAGE D'UNE MODALE À L'AUTRE////////////////////////////////////
 function nextModal() {
     const hiddenModal = document.querySelectorAll(".hidden-mod");
     const visibleModal = document.querySelectorAll(".visible-mod");
@@ -203,43 +235,48 @@ nextModal();
 
 
 
-//****SUPPRESSION PROJET/PHOTO ******//
+//////////////////////////////////////////////SUPPRESSION PROJET/PHOTO //////////////////////////////
  
+//Utilisation de fetch, pour communiquer avec l'api et supprimer les projets
+
     function deleteProjet(workId) {
         fetch(`http://localhost:5678/api/works/${workId}`, {
               method: "DELETE",
               headers: {
                 "accept": "application/json",
+                //on utilise bearer pour indiquer qu'on va utiliser le jeton pour s'authentifier
                 "Authorization": "Bearer " + localStorage.token,
              }
          })
             .then(response => {
                 if (!response.ok) {
                     console.log("Error: work not deleted")
-                }
+                } else {
 
-                const deleteModalWork = document.querySelector(`figure[data-id="${workId}"]`);
-                if (deleteModalWork) {
-                    deleteModalWork.remove();
+                    const deleteModalWork = document.querySelector(`figure[data-id="${workId}"]`);
+                    if (deleteModalWork) {
+                        deleteModalWork.remove();
 
-                    const deleteGalleryWork = document.querySelector(`figure[data-id="${workId}"]`);
-                    if (deleteGalleryWork) {
-                        deleteGalleryWork.remove();
-                    } else {
-                        console.log("Work not found in gallery");
-                    } 
+                        const deleteGalleryWork = document.querySelector(`figure[data-id="${workId}"]`);
+                        if (deleteGalleryWork) {
+                            deleteGalleryWork.remove();
+                        } else {
+                            console.log("Work not found in gallery");
+                        } 
+                    }
                 }
             })
             .catch(error => console.log(error))
        }
 
 
-//Gestion du background du bouton valider lors de l'envoi du formulaire
+///////////////////////Gestion du background du bouton valider lors de l'envoi du formulaire///////////////////////
 const titleWork = document.querySelector(".labelTitle");
 const categoryWork = document.getElementById("category");
 const imageWork = document.getElementById("photo");
 const btnvalidate = document.getElementById("btnValider");
 
+//on vérifie si les éléments du formulaire ne sont pas vides, pour modifier la couleur du bouton ou non
 function validateBtn() {
     if (titleWork.value !== "" && categoryWork.value !== "" && imageWork.value !== "") {
         btnvalidate.style.backgroundColor = "#1d6154";
@@ -257,7 +294,8 @@ imageWork.addEventListener("change", validateBtn);
 
 
 
-//Ajout d'un nouveau projet 
+///////////////////////////////////////////////// AJOUT D'UN NOUVEAU PROJET ///////////////////////////////////////
+
 const btnValiderAjout = document.getElementById("btnValider");
 btnValiderAjout.addEventListener("click", ajoutProjet);
 
@@ -307,10 +345,7 @@ function ajoutProjet(event) {
 
 
 
-
-
-
-//Gestion du Logout
+//////////////////Gestion du Logout en supprimant le token lorsqu'on clique sur logout///////////////////////
 
 const logouteve = document.querySelector("#logout");
 logouteve.addEventListener("click", function() {
