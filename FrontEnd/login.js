5f299c5974512e913e267aa45cb2e5907047639e
Clmt2707 <clement.deboeuf@gmail.com>
2024-01-01 11:07:09 +0100

    ajout de commentaires sur les fichiers de login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,12 +1,15 @@
 //******** Envoi formulaire de login ************
 
+//Les erreurs sont gérées en renvoyant un pop up à l'écran (si erreur)
+
+
 function ajoutListenerLogin() {
     const formulaireLogin = document.querySelector(".formulaireLogin");
     formulaireLogin.addEventListener("submit", async function(event) {
         event.preventDefault();
 
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
+        let email = document.getElementById("email");
+        let password = document.getElementById("password");
         
         //vérification de la validité de l'email via regexp
        let re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -16,16 +19,19 @@ function ajoutListenerLogin() {
             alert("L'email n'est pas valide");
         }
 
+        //gestion d'erreur si il manque un champ
         if (email.value === "" || password.value === "") {
             alert("Veuillez renseigner tous les champs.");
             event.preventDefault();
         }
 
-        //Vérification de l'authentification du user, appel à l'api 
+        //Vérification de l'authentification du user, appel à l'api avec la méthode POST
+        //création du body(charge utile)
         const rep = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-type": "application/json"},
-            //création de la charge utile
+            //création de la charge utile, on transforme les valerus d'email et password en chaine
+            //de caractère via JSON.stringify
             body: JSON.stringify({
                 email: email.value,
                 password: password.value,
@@ -49,4 +55,5 @@ function ajoutListenerLogin() {
 });
 }
 
+//Appel de la fonction pour se Login
 ajoutListenerLogin();
